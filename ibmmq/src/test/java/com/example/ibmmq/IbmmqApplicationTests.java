b1cb676489fbc85ab68579bb61c81e2f820bdfd6
Evangelos Vatikiotis <evangelos.vatikiotis@capgemini.com>
2021-06-05 12:59:51 +0300

    initial_commit

---
+++ ibmmq/src/test/java/com/example/ibmmq/IbmmqApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ibmmq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IbmmqApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
