3c04af042475fc305afd537c23b84b6c0506d177
Evangelos Vatikiotis <evangelos.vatikiotis@capgemini.com>
2021-06-05 19:55:15 +0300

    commented mqConnection different way of conf

--- ibmmq/src/main/java/com/example/ibmmq/IbmmqApplication.java
+++ ibmmq/src/main/java/com/example/ibmmq/IbmmqApplication.java
@@ -12,4 +12,15 @@ public class IbmmqApplication {
         SpringApplication.run(IbmmqApplication.class, args);
     }
 
+//    @Bean
+//    public Connection mqConnection() throws MalformedURLException, JMSException {
+//        MQQueueConnectionFactory mqQueueConnectionFactory = new MQQueueConnectionFactory();
+//        java.net.URL ccdt = new URL("file:///Users/evangelosvatikiotis/Documents/workspace/private_space/gs-messaging-jms/ibmmq/src/main/resources/AMQCLCHL.TAB");
+//        mqQueueConnectionFactory.setIntProperty(CommonConstants.WMQ_CONNECTION_MODE, CommonConstants.WMQ_CM_CLIENT);
+//
+//        mqQueueConnectionFactory.setChannel("DEV.ADMIN.SVRCONN");
+//        mqQueueConnectionFactory.setQueueManager("QM1");
+//        mqQueueConnectionFactory.setConnectionNameList("localhost(1414)");
+//        return mqQueueConnectionFactory.createConnection(username, password);
+//    }
 }
